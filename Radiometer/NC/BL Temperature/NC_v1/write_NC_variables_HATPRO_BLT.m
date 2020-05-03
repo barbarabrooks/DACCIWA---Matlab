8b7119aa4fa728a4e424dee687ef9efae336e0a7
barbarabrooks <61512749+barbarabrooks@users.noreply.github.com>
2020-05-03 09:23:36 +0100

    Add files via upload

---
+++ Radiometer/NC/BL Temperature/NC_v1/write_NC_variables_HATPRO_BLT.m
@@ -0,0 +1,353 @@
+function write_NC_variables_HATPRO_BLT(fn_nc, data, meta)
+%time
+        ts.Name='time';
+        ts.Nctype='double';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='seconds since 1970-01-01 00:00:00 UTC';
+        as(2).Name='standard_name';as(2).Value='time';
+        as(3).Name='long_name';as(3).Value='Time (seconds since 1970-01-01)';
+        as(4).Name='axis';as(4).Value='T';
+        as(5).Name='valid_min';as(5).Value=min(data.ET);
+        as(6).Name='valid_max';as(6).Value=max(data.ET);
+        clear ix
+        as(7).Name='calendar';as(7).Value='julian';
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc  
+%altitude
+        ts.Name='altitude';
+        ts.Nctype='double';
+        dc=cell(1);dc{1}='altitude';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='m';
+        as(2).Name='standard_name';as(2).Value='altitude';
+        as(3).Name='long_name';as(3).Value='Geometric height above geoid.';
+        as(4).Name='_FillValue';as(4).Value=-1e+020;
+        as(5).Name='axis';as(5).Value='Z';
+        ix=strfind(meta.l34,'m');
+        x=str2num(meta.l34(1:ix-1));
+        as(6).Name='valid_min';as(6).Value=min(data.Z(1,:))+x;
+        as(7).Name='valid_max';as(7).Value=max(data.Z(1,:))+x;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc 
+%latitude
+        ts.Name='latitude';
+        ts.Nctype='double';
+        dc=cell(1);dc{1}='latitude';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='degree_north';
+        as(2).Name='standard_name';as(2).Value='latitude';
+        as(3).Name='long_name';as(3).Value='Latitude';
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%longitude
+        ts.Name='longitude';
+        ts.Nctype='double';
+        dc=cell(1);dc{1}='longitude';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='degree_east';
+        as(2).Name='standard_name';as(2).Value='longitude';
+        as(3).Name='long_name';as(3).Value='Longitude';
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc       
+%alt_time
+        ts.Name='alt_time';
+        ts.Nctype='double';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='seconds since 0000-01-01 00:00:00 UTC';
+        as(2).Name='standard_name';as(2).Value='time';
+        as(3).Name='long_name';as(3).Value='Time (seconds since 0000-01-01)';
+        as(4).Name='valid_min';as(4).Value=min(data.ST);
+        as(5).Name='valid_max';as(5).Value=max(data.ST);
+        clear ix
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%day_of_year
+        ts.Name='day_of_year';
+        ts.Nctype='double';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value=['Day number from: ',num2str(data.DT(1,1)),'-01-01 00:00:00 UTC'];
+        as(2).Name='long_name';as(2).Value='Day of Year';
+        as(3).Name='valid_min';as(3).Value=min(data.DoY);
+        as(4).Name='valid_max';as(4).Value=max(data.DoY);
+        clear ix
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%year
+        ts.Name='year';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Year';
+        as(3).Name='valid_min';as(3).Value=min(data.DT(:,1));
+        as(4).Name='valid_max';as(4).Value=max(data.DT(:,1));
+        clear ix
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%month
+        ts.Name='month';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Month';
+        as(3).Name='valid_min';as(3).Value=min(data.DT(:,2));
+        as(4).Name='valid_max';as(4).Value=max(data.DT(:,2));
+        clear ix
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%day
+        ts.Name='day';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Day';
+        as(3).Name='valid_min';as(3).Value=min(data.DT(:,3));
+        as(4).Name='valid_max';as(4).Value=max(data.DT(:,3));
+        clear ix
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%hour
+        ts.Name='hour';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Hour';
+        as(3).Name='valid_min';as(3).Value=min(data.DT(:,4));
+        as(4).Name='valid_max';as(4).Value=max(data.DT(:,4));
+        clear ix
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%minute
+        ts.Name='minute';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Minute';
+        as(3).Name='valid_min';as(3).Value=min(data.DT(:,5));
+        as(4).Name='valid_max';as(4).Value=max(data.DT(:,5));
+        clear ix
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%second
+        ts.Name='second';
+        ts.Nctype='double';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Second';
+        as(3).Name='valid_min';as(3).Value=min(data.DT(:,6));
+        as(4).Name='valid_max';as(4).Value=max(data.DT(:,6));
+        clear ix
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc     
+              
+%air_temperature
+        ts.Name='air_temperature';
+        ts.Nctype='double';
+        dc=cell(1,2);dc{1}='time';dc{2}='altitude';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='K';
+        as(2).Name='standard_name';as(2).Value='air_temperature';
+        as(3).Name='long_name';as(3).Value='Temerature';
+        as(4).Name='_FillValue';as(4).Value=-1e+020;
+        as(5).Name='valid_min';as(5).Value=min(min(data.T));
+        as(6).Name='valid_max';as(6).Value=max(max(data.T));
+        as(7).Name='cell_methods';as(7).Value='time: mean';
+        as(8).Name='coordinates';as(8).Value='latitude longitude';
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc  
+        
+%qc_flag_temperature
+        ts.Name='qc_flag_temperature';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Temperature';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc        
+%qc_flag_relative_humidity
+        ts.Name='qc_flag_relative_humidity';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Humidity';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+ %qc_flag_pressure
+        ts.Name='qc_flag_pressure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Pressure';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+ %qc_flag_precipitation
+        ts.Name='qc_flag_precipitation';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Rainfall Rate and Accumulated rain';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+ %qc_flag_channel_1_failure
+        ts.Name='qc_flag_channel_1_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 1 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+ %qc_flag_channel_2_failure
+        ts.Name='qc_flag_channel_2_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 2 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+ %qc_flag_channel_3_failure
+        ts.Name='qc_flag_channel_3_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 3 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_4_failure
+        ts.Name='qc_flag_channel_4_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 4 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_5_failure
+        ts.Name='qc_flag_channel_5_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 5 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_6_failure
+        ts.Name='qc_flag_channel_6_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 6 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_7_failure
+        ts.Name='qc_flag_channel_7_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 7 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_8_failure
+        ts.Name='qc_flag_channel_8_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 8 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_9_failure
+        ts.Name='qc_flag_channel_9_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 9 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_10_failure
+        ts.Name='qc_flag_channel_10_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 10 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_11_failure
+        ts.Name='qc_flag_channel_11_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 11 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_12_failure
+        ts.Name='qc_flag_channel_12_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 12 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_13_failure
+        ts.Name='qc_flag_channel_13_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 13 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_channel_14_failure
+        ts.Name='qc_flag_channel_14_failure';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Receiver channel 14 failed';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=3;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_t_receiver_temperature_stability
+        ts.Name='qc_flag_t_receiver_temperature_stability';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Thermal stability of temperature receiver bank';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=7;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
+%qc_flag_rh_receiver_temperature_stability
+        ts.Name='qc_flag_rh_receiver_temperature_stability';
+        ts.Nctype='int';
+        dc=cell(1);dc{1}='time';ts.Dimension=dc;
+        as(1).Name='units';as(1).Value='1';
+        as(2).Name='long_name';as(2).Value='Data Qulaity flag: Thermal stability of moisture receiver bank';
+        as(3).Name='valid_min';as(3).Value=1;
+        as(4).Name='valid_max';as(4).Value=7;
+        ts.Attribute=as;
+        nc_addvar(fn_nc,ts);clear ts as dc
---
+++ Radiometer/NC/BL Temperature/NC_v3/NC_HATPRO_BLT_v3.m
@@ -0,0 +1,60 @@
+function NC_HATPRO_BLT_v3(dn,do,fn_meta)
+
+%fn_meta='C:\Users\lecbjb\Desktop\Data Project\Stationary\ncas-scanning-radiometer-1\surface-met\Example META Data File\Meta data.csv';
+% Wrapper to create NC files for pro
+%input
+%   dn: directory holding the matlab files
+%   do: directory for nc files
+%   fn_meta: full path to project meta file
+%
+% ncas-aws-<n>_<platform_name>_<YYYYMMDD>_<data_product>_<version>.nc
+
+%BB 24/06/2017
+%fn_meta='E:\ACSE - radiometer\meta\boundary-layer-temeprature-profiles\ACSE_meta_bound_lay_temp_v2.txt';
+%dn='E:\ACSE - radiometer\matlab';
+%do='E:\ACSE - radiometer\NC\V1\boundary-layer-temperature-profiles';
+%get meat data
+meta = get_meta_v3(fn_meta);
+
+%extract whats needed to create file name
+[instrument_name,platform,data_product,version,scan_type] = NC_file_name_parts_v3(meta);
+
+%get list of what is to be cnverted
+list=dir(dn);
+
+for n=3:length(list)
+    %get file time stamp
+    temp=list(n).name;
+    ix=strfind(temp,'.');
+    timestamp=temp(1:ix-1);%timestamp
+    clear temp
+    
+    %nc file name and path
+    fc=fullfile (do,[instrument_name,'_',platform,'_',timestamp,'_',data_product,'_v',version,'.nc']);
+    
+    display(list(n).name)
+    
+    %load data file
+    load(fullfile(dn,list(n).name));
+    
+    if ~isempty(data.TPB)
+        data1=data;clear data
+        data=data1.TPB;clear data1
+       
+        %create nc file, assumes NC_CLOBBER
+        nc_create_empty(fc);
+        %global attributes
+        write_NC_header_v3(fc,meta,data); 
+        %Dimensions
+        write_NC_dimensions_HATPRO_BLT_v3(fc,meta,data);        
+        %Variables - General - generic function
+        write_NC_variables_general_v3(fc,meta,data);
+        %Variables - specific - specific function
+        write_NC_variables_specific_HATPRO_BLT_v3(fc,meta);
+        %Data    
+        write_NC_data_HATPRO_BLT_v3(fc,meta,data);
+    end
+    %clear fc temp ix f3 data data1
+end
+
+%end
